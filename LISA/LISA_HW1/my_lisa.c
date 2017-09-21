159d9f74c61ffddb7df23c85e986258b266a4328
Carlos Davila <carlos.davila@sjsu.edu>
2017-09-21 00:29:28 -0700

    Finished fixing some bugs. Working on new algorithms. Need to also do testing automated framework.

--- LISA/LISA_HW1/my_lisa.c
+++ LISA/LISA_HW1/my_lisa.c
@@ -3,15 +3,11 @@
  *
  *  Created on: Sep 16, 2017
  *      Author: Carlos R. Davila
+ *
+ *      Status: Working version. Tests and robustness still WIP.
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
-#define LISA_SYNC_LEN 32
-#define BUFFER_LEN 1024
-#define DEBUG 0
+#include "my_lisa.h"
 
 /**
  * Function will combine lisa_sync + payload and place it in the
@@ -23,8 +19,8 @@
  * @param gen_file  Whether to generate an output file [1] or not [0]. The
  *                  name of the output file will be lisa_output.txt
  */
-int gen_output_buffer(char * buffer, char * lisa_sync,
-        char * payload) {
+int gen_output_buffer(unsigned char * buffer, unsigned char * lisa_sync,
+		char * payload) {
 
     int lisa_idx, payload_idx;
     int payload_len = strlen(payload);
@@ -66,7 +62,7 @@ uint gen_mask(uint position) {
  * @param corrupt_pct       Percentage of bits to corrupt in LISA buffer (0-100)
  * @param lisa_sync_buffer  Buffer to save sync field to
  */
-void generate_lisa_sync(int corrupt_pct, char * lisa_sync_buffer) {
+void generate_lisa_sync(int corrupt_pct, unsigned char * lisa_sync_buffer) {
     char lisa_prefixes [2] = {0xA0, 0x50};
     int idx;
 
@@ -145,13 +141,17 @@ void pct_prompt(int * input_pct, char * prompt) {
 }
 
 /**
+ * Standard LISA algorithm discussed in class. Will implement custom ones for
+ * fun to test speed and other reliability improvements.
  *
+ * @param confidence_lvl	confidence level for the search algorithm to test
+ * @param input_buffer		buffer to search for the LISA sync and payload
  */
-int lisa_find_payload_vanilla(int confidence_lvl, char * input_buffer) {
+int lisa_find_payload_vanilla(int confidence_lvl, unsigned char * input_buffer) {
     int num_matched, max_match_idx = 0;
     double max_matched_pct = 0.0;
     int window_match_pcts[BUFFER_LEN - LISA_SYNC_LEN] = {0};
-    char lisa_buffer [LISA_SYNC_LEN];   // Generate and hold genuine LISA field
+    unsigned char lisa_buffer [LISA_SYNC_LEN];   // Gen and hold good LISA field
 
     generate_lisa_sync(0, lisa_buffer);
 
@@ -168,12 +168,72 @@ int lisa_find_payload_vanilla(int confidence_lvl, char * input_buffer) {
     }
 
     max_matched_pct = (double)window_match_pcts[max_match_idx] / LISA_SYNC_LEN;
+    printf("[INFO] Match at (%d) with confidence: %.2f%%\n",
+    		max_match_idx + LISA_SYNC_LEN, max_matched_pct * 100);
     if(max_matched_pct >= confidence_lvl) {
-        // Calculate the position based on the most occuring offset
-        printf("[INFO] Match with confidence: %.2f%%\n", max_matched_pct * 100);
         return max_match_idx + LISA_SYNC_LEN;
     } else
         return 0;
+}
+
+int prob_match(int offset, unsigned char * buffer,
+		unsigned char * lisa_buffer) {
+	int matched [2] = {0};
+	int idx = 0;
+
+	for(int i = 0; i < LISA_SYNC_LEN; i++){
+		if(buffer[offset + i] == lisa_buffer[i]) {
+			matched[idx]++;
+			if(matched[0] >= 3 && idx == 0) {
+				i += 0xAF - lisa_buffer[i];
+				idx++;
+			} else if(matched[1] >= 3) break;
+		}
+	}
+
+	if(matched[1] < 3)
+		return -1;
+	else
+		return offset + 32;
+}
+
+/**
+ * New LISA algorithm. With two key improvements:
+ * 	1) Searches with non-overlapping windows
+ * 	2) Limited comparisons upon match with high confidence
+ *
+ * @param confidence_lvl	confidence level for the search algorithm to test
+ * @param input_buffer		buffer to search for the LISA sync and payload
+ */
+int lisa_find_payload_prob(int confidence_lvl, unsigned char * input_buffer) {
+    int matched = 0;
+    int sub = 0;
+    unsigned char lisa_buffer [LISA_SYNC_LEN];   // Gen and hold good LISA field
+
+    generate_lisa_sync(0, lisa_buffer);
+
+    // Window method
+    for(int i = 0; i < BUFFER_LEN - LISA_SYNC_LEN; i++) {
+        for(int j = 0; j < LISA_SYNC_LEN; j++) {
+            if(input_buffer[i + j] == lisa_buffer[j]) {
+                //launch search alg
+            	if(input_buffer[i + j] >> 4 == 0xA)
+            		sub = 0xA0;
+            	else
+            		sub = 0x50;
+
+            	int lisa_start_loc = (i + j) - (input_buffer[i + j] - sub);
+            	matched = prob_match(lisa_start_loc, input_buffer, lisa_buffer);
+            	if(matched != -1) {
+            		i = j = BUFFER_LEN;
+            		break;
+            	}
+            }
+        }
+    }
+
+    printf("[INFO] Matched using prob alg!\n");
+    return matched;
 }
 
 /**
@@ -181,25 +241,20 @@ int lisa_find_payload_vanilla(int confidence_lvl, char * input_buffer) {
  *  
  *  @param payload_idx_in
  */
-void read_file_buffer(int * payload_idx_input,
-        char * file_location, int confidence_lvl) {
-
-    char buffer[BUFFER_LEN];
-    char line_buffer[1];
+void read_file_buffer(unsigned char * buffer, char * file_location) {
+//    char buffer[BUFFER_LEN];
+    char line_buffer[1] = {0};
     FILE *fp;
 
     fp = fopen(file_location, "r");
-
     if(fp) {
         for(int i = 0; i < BUFFER_LEN; i++) {
-            fgets(line_buffer, 4, (FILE*)fp);
-            buffer[i] = line_buffer[0];
-            printf("Line[%d]: %.2x\n", i, (unsigned char)buffer[i]);
+            if(fgets(line_buffer, 2, (FILE*)fp) != NULL);
+            	buffer[i] = line_buffer[0];
+            if(DEBUG >= 1 && i > 370 && i < 400)
+            	printf("RLine[%d]: %.2x\n", i, (unsigned char)buffer[i]);
         }
         fclose(fp);
-        *payload_idx_input = lisa_find_payload_vanilla(confidence_lvl, buffer);
-    } else {
-        *payload_idx_input = NULL;
     }
 }
 
@@ -210,44 +265,50 @@ void read_file_buffer(int * payload_idx_input,
  * @param buffer    buffer to write to a file
  * @param filename  filename to write to
  */
-void write_file_buffer(char * buffer, char * file_location) {
+void write_file_buffer(unsigned char * buffer, char * file_location) {
     // Read file and stuff
     FILE *fp;
-    fp = fopen(file_location, "w+");
+    fp = fopen(file_location, "w");
 
     if(fp) {
         for(int i = 0; i < BUFFER_LEN; i++) {
-            fprintf(fp, "%c\n", (unsigned char)buffer[i]);
+            fprintf(fp, "%c", (unsigned char)buffer[i]);
+            if(DEBUG >= 1 && i > 370 && i < 400)
+            	printf("WLine[%d]:%.2x\n", i, (unsigned char)buffer[i]);
         }
         fclose(fp);
     }
 }
 
-void main() {
-    time_t t;
-    srand((unsigned) time(&t));  // seed for repeatable results
-    srand(97);  // seed for repeatable results
-
-    int usr_input, payload_idx_output, payload_idx_input;
-    char lisa_sync_buffer [LISA_SYNC_LEN] = {0};
-    char output_buffer [BUFFER_LEN];
-    char * payload = "Hello LISA!";
-
-    // Generate output
-    pct_prompt(&usr_input, "%% LISA Sync Field Corruption? [0-100]: ");
-    generate_lisa_sync(usr_input, lisa_sync_buffer);
-    payload_idx_output = gen_output_buffer(output_buffer,
-            lisa_sync_buffer, payload);
-    write_file_buffer(output_buffer, "lisa_output.txt");
-
-    // Read input buffer
-    pct_prompt(&usr_input, "%% Confidence level to apply [0-100]: ");
-    read_file_buffer(&payload_idx_input, "lisa_output.txt", usr_input);
-
-    if(payload_idx_output == payload_idx_input) {
-        printf("[SUCCESS] Payload found at index %d: %s", payload_idx_input,
-                &output_buffer[payload_idx_input]);
-    } else
-        printf("[ERROR] Failed to find payload.\n [o]=%d != [i]=%d\n",
-                payload_idx_output, payload_idx_input);
-}
+//void main() {
+//    time_t t;
+//    srand((unsigned) time(&t));  // seed for repeatable results
+//    srand(97);  // seed for repeatable results
+//
+//    int corruption_pct, match_confidence, payload_idx_output, payload_idx_input;
+//    unsigned char lisa_sync_buffer [LISA_SYNC_LEN] = {0};
+//    unsigned char output_buffer [BUFFER_LEN];
+//    unsigned char input_buffer [BUFFER_LEN];
+//    char * payload = "Hello LISA!";
+//
+//    // Generate output
+//    pct_prompt(&corruption_pct, "%% LISA Sync Field Corruption? [0-100]: ");
+//    generate_lisa_sync(corruption_pct, lisa_sync_buffer);
+//    payload_idx_output = gen_output_buffer(output_buffer,
+//            lisa_sync_buffer, payload);
+//    write_file_buffer(output_buffer, "lisa_output.txt");
+//
+//    // Read input buffer
+//    pct_prompt(&match_confidence, "%% Confidence level to apply [0-100]: ");
+//    read_file_buffer(input_buffer, "lisa_output.txt");
+//    payload_idx_input = lisa_find_payload_vanilla(match_confidence,
+//    		input_buffer);
+//
+//    if(payload_idx_output == payload_idx_input) {
+//        printf("[SUCCESS] Payload found at index %d: %s",
+//        		payload_idx_input, &output_buffer[payload_idx_input]);
+//    } else
+//        printf("[ERROR] Failed to find payload with confidence >= %d%%."
+//        		" [(E)%d , (R)%d]", match_confidence, payload_idx_output,
+//				payload_idx_input);
+//}
